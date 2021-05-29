b8e1539e83c1029a22c510ede0ab887eba99bf08
zhongshengqin <zhongshengqin@tvdcy.net>
2021-05-29 19:56:35 +0800

    整理包的关系

--- common-center/src/main/java/com/sachzhong/common/CommonCenterApplication.java
+++ common-center/src/main/java/com/sachzhong/common/CommonCenterApplication.java
@@ -2,14 +2,12 @@ package com.sachzhong.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  @Author: SachZhong
  @Description:
  @Date Created in 2021/5/29 16:05
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CommonCenterApplication {
 
     public static void main(String[] args) {
